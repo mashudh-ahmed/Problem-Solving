3e202bf4465240e7806fc8da09670f41805af639
Mashudh Ahmed <mashudh.ahmed@outlook.com>
2024-03-25 01:24:08 +0600

    Add files via upload

---
+++ Codeforces/1791A -  Codeforces Checking.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    while (n--) {
+        char ch;
+        cin >> ch;
+
+    if (ch == 'c' || ch == 'o' || ch == 'd' || ch == 'e' || ch == 'f' || ch == 'o' || ch == 'r' || ch == 'c' || ch == 'e' || ch == 's')
+     {
+         cout<<"Yes"<<endl;
+     }
+     else
+     {
+
+         cout<<"No"<<endl;
+     }
+
+
+    }
+
+    return 0;
+}
